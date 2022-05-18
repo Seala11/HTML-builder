e21d5d1a7083f6e705901428672313cf2b3d029f
Hanna Papova <seal.annuska@gmail.com>
2022-05-17 20:12:53 -0400

    feat: add copy directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+// import modules
+const path = require('path');
+const { copyFile, mkdir, readdir } = require('fs/promises');
 
+// set the path to the src/dist folders
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+// 1. create dist folder if necessary
+const createDistFolder = async () => {
+  try {
+    await mkdir(destDir, { recursive: true });
+    // start reading files
+    readFiles();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// 2. read files from src folder
+const readFiles = async () => {
+  try {
+    const files = await readdir(srcDir, { withFileTypes: true });
+    // pass each file to copy
+    for (const file of files) {
+      await copyDir(file.name);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// 3. copy files to dist folder
+const copyDir = async (fileName) => {
+  const fromPath = path.join(srcDir, fileName);
+  const toPath = path.join(destDir, fileName);
+
+  try {
+    await copyFile(fromPath, toPath);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+createDistFolder();
