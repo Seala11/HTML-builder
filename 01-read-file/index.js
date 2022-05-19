b848f2f926a871a8a7037f328d2e0bed78b509cc
Hanna Papova <seal.annuska@gmail.com>
2022-05-19 17:04:24 -0400

    refactor: use pipeline

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,18 @@
 // import modules
 const { createReadStream } = require('fs');
+const { pipeline } = require('stream');
 const path = require('path');
-const process = require('process');
 
 // get the path to the text file
 const currPath = path.join(__dirname, 'text.txt');
 
-// create readable stream
-const readStream = createReadStream(currPath, { encoding: 'utf8' });
-
-// put handler on 'data' event
-readStream.on('data', (chunk) => {
-  process.stdout.write(chunk);
-});
+// create pipeline between read stream and write stream(console)
+pipeline(
+  createReadStream(currPath, { encoding: 'utf8' }),
+  process.stdout,
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed: ', err);
+    }
+  },
+);
