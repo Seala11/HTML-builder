95b238d748c7a7c9a461186cbaab34df722c7d21
Hanna Papova <seal.annuska@gmail.com>
2022-05-24 14:08:20 -0400

    fix: remove files before copy if files-copy folder exists

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,12 +1,41 @@
 // import modules
 const path = require('path');
-const { copyFile, mkdir, readdir } = require('fs/promises');
+const { copyFile, mkdir, readdir, access, unlink } = require('fs/promises');
 
 // set the path to the src/dist folders
 const srcDir = path.join(__dirname, 'files');
 const destDir = path.join(__dirname, 'files-copy');
 
-// 1. create dist folder if necessary
+// 1. check if folder exist
+const checkDistFolder = async () => {
+  try {
+    // 2.A: if files-copy folder exists
+    await access(destDir);
+    removeFiles();
+  } catch {
+    // 2.B: if files-copy folder doesn`t exist
+    createDistFolder();
+  }
+};
+
+// 2.A: reset files-copy if folder existed
+const removeFiles = async () => {
+  try {
+    const files = await readdir(destDir, { withFileTypes: true });
+    // remove each file
+    for (const file of files) {
+      if (file.isFile()) {
+        unlink(path.join(destDir, file.name));
+      }
+    }
+    // copy files
+    readFiles();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// 2.B: create dist folder if necessary
 const createDistFolder = async () => {
   try {
     await mkdir(destDir, { recursive: true });
@@ -17,7 +46,7 @@ const createDistFolder = async () => {
   }
 };
 
-// 2. read files from src folder
+// 3. read files from src folder
 const readFiles = async () => {
   try {
     const files = await readdir(srcDir, { withFileTypes: true });
@@ -32,7 +61,7 @@ const readFiles = async () => {
   }
 };
 
-// 3. copy files to dist folder
+// 4. copy files to dist folder
 const copyDir = async (fileName) => {
   const fromPath = path.join(srcDir, fileName);
   const toPath = path.join(destDir, fileName);
@@ -44,4 +73,4 @@ const copyDir = async (fileName) => {
   }
 };
 
-createDistFolder();
+checkDistFolder();
